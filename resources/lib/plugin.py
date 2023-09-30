e7491429598d3364444ca0c44db4f448b5a661da
Paco8 <5084042+Paco8@users.noreply.github.com>
2023-09-30 15:20:15 +0200

    Version 0.4.8

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -529,25 +529,34 @@ def router(paramstring):
     if sky.logged:
       for item in sky.get_main_menu():
         if item['id'] == 'My Stuff':
-          add_menu_option(item['title'], get_url(action='wishlist')) # My list
+          add_menu_option(item['title'], get_url(action='wishlist'), icon='mylist.png') # My list
         elif item['id'] == 'Channels':
-          add_menu_option(item['title'], get_url(action='tv')) # TV
+          add_menu_option(item['title'], get_url(action='tv'), icon='tv.png') # TV
         else:
           art = None
           #if item.get('icon'): art={'icon': item['icon']}
           add_menu_option(item['title'], get_url(action='category', name=item['title'], slug=item['slug']), art=art)
 
-      add_menu_option(addon.getLocalizedString(30122), get_url(action='continue-watching')) # Continue watching
-      add_menu_option(addon.getLocalizedString(30112), get_url(action='search')) # Search
-      add_menu_option(addon.getLocalizedString(30180), get_url(action='profiles')) # Profiles
+      add_menu_option(addon.getLocalizedString(30122), get_url(action='continue-watching'), icon='continue.png') # Continue watching
+      add_menu_option(addon.getLocalizedString(30112), get_url(action='search'), icon='search.png') # Search
+      add_menu_option(addon.getLocalizedString(30180), get_url(action='profiles'), icon='profiles.png') # Profiles
       #add_menu_option(addon.getLocalizedString(30108), get_url(action='devices')) # Devices
 
     #add_menu_option(addon.getLocalizedString(30160), get_url(action='user')) # Accounts
-    add_menu_option(addon.getLocalizedString(30160), get_url(action='platforms')) # Accounts
+    add_menu_option(addon.getLocalizedString(30160), get_url(action='platforms'), icon='account.png') # Accounts
     close_folder(cacheToDisc=False)
 
 
 def run():
+  # Call the router function and pass the plugin call parameters to it.
+  # We use string slicing to trim the leading '?' from the plugin call paramstring
+  params = sys.argv[2][1:]
+
+  if 'show_donation_dialog' in params:
+    from .customdialog import show_donation_dialog
+    show_donation_dialog()
+    return
+
   global sky
   LOG('profile_dir: {}'.format(profile_dir))
   platform_id = addon.getSetting('platform_id').lower()
@@ -562,7 +571,4 @@ def run():
   # Clear cache
   LOG('Cleaning cache. {} files removed.'.format(sky.cache.clear_cache()))
 
-  # Call the router function and pass the plugin call parameters to it.
-  # We use string slicing to trim the leading '?' from the plugin call paramstring
-  params = sys.argv[2][1:]
   router(params)
