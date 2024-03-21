f52e73dfa65696c606bf843b9552952d44f2094e
Paco8 <5084042+Paco8@users.noreply.github.com>
2024-03-21 14:57:03 +0100

    Version 0.4.13

--- resources/lib/sky.py
+++ resources/lib/sky.py
@@ -408,9 +408,8 @@ class SkyShowtime(object):
       headers['content-type'] = 'application/x-www-form-urlencoded'
       headers['Accept'] = 'application/vnd.siren+json'
       #headers['User-Agent'] = chrome_user_agent
-      #del headers['x-skyott-device']
+      del headers['x-skyott-device']
       del headers['x-skyott-platform']
-      headers['x-skyott-device'] = 'PC'
       print_json(headers)
 
       post_data = {'userIdentifier': username, 'password': password, 'rememberMe': True, 'isWeb': True}
